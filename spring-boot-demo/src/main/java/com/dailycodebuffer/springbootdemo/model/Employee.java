a1e6802376655ece24cddf0c22016e07b4833697
Alin Nedelcu <anedelcu104@gmail.com>
2023-03-08 12:47:39 -0600

    Initial commit

---
+++ spring-boot-demo/src/main/java/com/dailycodebuffer/springbootdemo/model/Employee.java
@@ -0,0 +1,10 @@
+package com.dailycodebuffer.springbootdemo.model;
+
+public class Employee {
+
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String department;
+}
