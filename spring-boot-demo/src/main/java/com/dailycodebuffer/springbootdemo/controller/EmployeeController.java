28610670e89f55c8dccd8a87bded2de2916a31ff
Alin Nedelcu <anedelcu104@gmail.com>
2023-03-08 19:26:56 -0600

    Spring data JPA with Spring Boot

--- spring-boot-demo/src/main/java/com/dailycodebuffer/springbootdemo/controller/EmployeeController.java
+++ spring-boot-demo/src/main/java/com/dailycodebuffer/springbootdemo/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.dailycodebuffer.springbootdemo.model.Employee;
 import com.dailycodebuffer.springbootdemo.service.EmployeeService;
 import com.dailycodebuffer.springbootdemo.service.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 @RequestMapping("/v1/employees")
 public class EmployeeController {
 
+    @Qualifier("employeeServiceImpl")
     @Autowired
     private EmployeeService employeeService;
 
