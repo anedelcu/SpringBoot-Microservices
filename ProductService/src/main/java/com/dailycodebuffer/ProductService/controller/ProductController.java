35e83e7d668f50335993e118c7fb680825326e6b
Alin Nedelcu <anedelcu104@gmail.com>
2023-03-23 11:13:53 -0500

    Config server.

--- ProductService/src/main/java/com/dailycodebuffer/ProductService/controller/ProductController.java
+++ ProductService/src/main/java/com/dailycodebuffer/ProductService/controller/ProductController.java
@@ -29,4 +29,15 @@ public class ProductController {
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 
+    @PutMapping("/reduceQuantity/{id}")
+    public ResponseEntity<Void> reduceQuantity(@PathVariable ("id") long productId,
+                                               @RequestParam long quantity) {
+
+        productService.reduceQuantity(productId, quantity);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+
+
+    }
+
 }
